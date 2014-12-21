89739b036ae7ef5b1937477466ca95c1d75a57be
Hannah Murray <hlmurray91@gmail.com>
2014-12-21 13:51:06 -0500

    add html and js src files

--- gulpfile.js
+++ gulpfile.js
@@ -21,18 +21,39 @@ var paths = {
 	}
 }
 
-// Gulp task
-gulp.task('compile-scss', function(){
-	return gulp.src(paths.src.scss+"/**/*.scss")
-			.pipe(sass({style: "compressed", noCache: true, 'sourcemap=none': true}))
-			.pipe(prefix("last 3 versions"))
-			.pipe(gulp.dest(paths.build.css));
+// Gulp tasks
+	// SCSS Compiler
+	gulp.task('compile-scss', function(){
+		return gulp.src(paths.src.scss+"/**/*.scss")
+		.pipe(sass({style: "compressed", noCache: true, 'sourcemap=none': true}))
+		.pipe(prefix("last 3 versions"))
+		.pipe(gulp.dest(paths.build.css));
 });
+
+	// JS Compiler
+	gulp.task("compile-js", function(){
+		return gulp.src(paths.src.js+"/**/*.js")
+		.pipe(gulp.dest(paths.build.js))
+	});
+
+	// HTML Compiler
+	gulp.task("compile-html", function(){
+		return gulp.src(paths.src.html+"/**/*.html")
+		.pipe(gulp.dest(paths.build.html))
+	});
+
 
 gulp.task('watch', function(){
 	// What to watch
 	gulp.watch(paths.src.scss+"/**/*.scss", ["compile-scss"]);
+	gulp.watch(paths.src.js+"/**/*.js", ["compile-js"]);
+	gulp.watch(paths.src.html+"/**/*.html", ["compile-html"]);
+});
+
+gulp.task("compile", ["compile-html","compile-js","compile-scss",], function(){
+	return gulp.src("src/fake")
+	.pipe(gulp.dest("src/fake"))
 });
 
 // Compile all files, watch all source files
-gulp.task("up", ["compile-scss","watch"], function(){});
+gulp.task("up", ["compile","watch"], function(){});

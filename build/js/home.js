ad1f0560b1299b68b14ca3f48bf3f249c27dc45d
Hannah <hlmurray91@gmail.com>
2015-03-25 19:37:31 -0400

    feat(master-group): include build folder

---
+++ build/js/home.js
@@ -0,0 +1,8 @@
+$('.home-hero > .home-slideshow').cycle({
+	speed: 450,
+    fx: 'scrollHorz',
+    slides: '> div.home-slides',
+    prev: '.pager-prev',
+    next: '.pager-next',
+    timeout: 0
+});

d9359c696e15efe5d8cd20ba0f37526b5dbddea4
Hannah <hlmurray91@gmail.com>
2015-04-14 22:32:26 -0400

    fix(parsonstable-redesign): continue integration

---
+++ src/js/contact.js
@@ -0,0 +1,37 @@
+function initialize() {
+	  var mapStyles = [
+		{
+		    "stylers": [
+		      { "saturation": -100 },
+		      { "lightness": 7 }
+		    ]
+		}
+	  ];
+	  var mapOptions = {
+	    zoom: 16,
+	    center: new google.maps.LatLng(33.875283, -78.616226),
+	    scrollwheel: false,
+	    draggable: true,
+	    panControl: true,
+	    scaleControl: false,
+	    zoomControl: true,
+	    streetViewControl: false,
+	    disableDoubleClickZoom: true,
+	    mapTypeId: 'Styled'
+	  };
+
+	  var map = new google.maps.Map(document.getElementById('map-canvas'),
+	      mapOptions);
+
+	  var styledMapType = new google.maps.StyledMapType(mapStyles, { name: 'Styled' });
+	  map.mapTypes.set('Styled', styledMapType);
+	}
+
+	function loadScript() {
+	  var script = document.createElement('script');
+	  script.type = 'text/javascript';
+	  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
+	      '&callback=initialize';
+	  document.body.appendChild(script);
+	}
+window.onload = loadScript;

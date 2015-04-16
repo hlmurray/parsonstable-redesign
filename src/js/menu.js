6e0177c39a3fb3d161619bd1c190d5bcfe90010b
Hannah <hlmurray91@gmail.com>
2015-04-15 22:46:15 -0400

    feat(parsonstable-redesign): continue integration

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,8 +1,25 @@
-$('.menu-nav a').click(function() {
-	var menuType = $(this).attr('class');
-	$(this).addClass('active');
-	$('.menu-nav').find('a.active').not(this).removeClass('active');
-	var activeMenu = $('.menu-right > div.'+menuType);
-	activeMenu.addClass('active');
-	$('.menu-right').find('div.active').not(activeMenu).removeClass('active');
+$(document).ready(function() {
+	var menuHeight = $('.menu-right'),
+		starterHeight = $('.menu-right > div.starters').outerHeight();
+
+	$('.menu-right').css({
+		'height': starterHeight
+	});
+
+	$('.menu-nav a').click(function() {
+		var menuType = $(this).attr('class'),
+			activeMenu = $('.menu-right > div.'+menuType),
+			starterHeight = activeMenu.outerHeight();
+
+		// CHANGE HEIGHT BASED ON MENU
+		$('.menu-right').css({
+			'height': starterHeight
+		});
+
+		// TOGGLE MENU CHANGE
+		$(this).addClass('active');
+		$('.menu-nav').find('a.active').not(this).removeClass('active');
+		activeMenu.addClass('active');
+		$('.menu-right').find('div.active').not(activeMenu).removeClass('active');
+	});
 });

fb5389ac485f98cec087cd0c6abe8c9d278bb102
Hannah <hlmurray91@gmail.com>
2015-04-19 21:57:22 -0400

    fix(parsonstable-redesign): menu improvements

--- build/js/menu.js
+++ build/js/menu.js
@@ -2,23 +2,28 @@ $(document).ready(function() {
 	var menuHeight = $('.menu-right'),
 		starterHeight = $('.menu-right > div.starters').outerHeight();
 
-	$('.menu-right').css({
+	menuHeight.css({
 		'height': starterHeight
 	});
 
 	$('.menu-nav a').click(function() {
+		if ($(this).hasClass('active')){
+			return false
+		}
+
 		var menuType = $(this).attr('class'),
 			activeMenu = $('.menu-right > div.'+menuType),
 			starterHeight = activeMenu.outerHeight();
 
 		// CHANGE HEIGHT BASED ON MENU
-		$('.menu-right').css({
+		menuHeight.css({
 			'height': starterHeight
 		});
-
+		
 		// TOGGLE MENU CHANGE
 		$(this).addClass('active');
 		$('.menu-nav').find('a.active').not(this).removeClass('active');
+		
 		activeMenu.addClass('active');
 		$('.menu-right').find('div.active').not(activeMenu).removeClass('active');
 	});

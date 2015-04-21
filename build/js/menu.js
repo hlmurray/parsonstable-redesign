a3154f07e1a942bbb5a3a0a4438811a166d12e32
Hannah <hlmurray91@gmail.com>
2015-04-20 22:44:33 -0400

    fix(menu): adjust wine list submenu

--- build/js/menu.js
+++ build/js/menu.js
@@ -6,25 +6,43 @@ $(document).ready(function() {
 		'height': starterHeight
 	});
 
-	$('.menu-nav a').click(function() {
-		if ($(this).hasClass('active')){
-			return false
-		}
-
-		var menuType = $(this).attr('class'),
-			activeMenu = $('.menu-right > div.'+menuType),
-			starterHeight = activeMenu.outerHeight();
-
-		// CHANGE HEIGHT BASED ON MENU
-		menuHeight.css({
-			'height': starterHeight
-		});
-		
-		// TOGGLE MENU CHANGE
-		$(this).addClass('active');
-		$('.menu-nav').find('a.active').not(this).removeClass('active');
-		
-		activeMenu.addClass('active');
-		$('.menu-right').find('div.active').not(activeMenu).removeClass('active');
-	});
+	$('.menu-nav > span').on('click', 'span', function() {
+        if ($(this).hasClass('active')){
+            return false
+        }
+ 
+        var menuType = $(this).parent().attr('class'),
+            activeMenu = $('.menu-right > div.'+menuType),
+            starterHeight = activeMenu.outerHeight();
+ 
+        // CHANGE HEIGHT BASED ON MENU
+        menuHeight.css({
+            'height': starterHeight
+        });
+        
+        // TOGGLE MENU CHANGE
+        $(this).addClass('active');
+        if(menuType == 'wine') {
+        	$('span.'+menuType+' ul').addClass('active');
+        } else {
+        	$('span.wine ul').removeClass('active');
+        }
+        $('.menu-nav').find('span.active').not(this).removeClass('active');
+        
+        activeMenu.addClass('active');
+        $('.menu-right').find('div.active').not(activeMenu).removeClass('active');
+    });
+ 
+    $('.menu-nav span > ul ').on('click', 'a', function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
 });

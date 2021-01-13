66ce3d731a125bd9681d4689f07c8e9c271fde15
hlmurray91 <hlmurray91@gmail.com>
2021-01-13 16:20:26 -0500

    Finish up home page

--- src/js/home.js
+++ src/js/home.js
@@ -1,7 +1,10 @@
 $('.home-carousel').slick({
   accessibility: true,
   arrows: false,
+  autoplay: true,
+  autoplaySpeed: 3500,
   centerMode: true,
   infinite: true,
+  speed: 500,
   variableWidth: true,
 });

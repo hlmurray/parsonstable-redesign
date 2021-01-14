b91557809e52ff31be30e39a9fd5c6386a99bc44
hlmurray91 <hlmurray91@gmail.com>
2021-01-14 18:27:29 -0500

    Make header/footer and hp responsive

--- src/js/home.js
+++ src/js/home.js
@@ -8,3 +8,10 @@ $('.home-carousel').slick({
   speed: 500,
   variableWidth: true
 });
+
+const toggleMenu = (e) => {
+  $('.nav-menu-icon').toggleClass('active');
+  $('.nav-menu').toggleClass('active');
+  $('.mobile-overlay').toggleClass('active');
+  $('body').toggleClass('mobile-menu-active');
+};
